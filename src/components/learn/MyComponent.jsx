1d450b23211fb058bb681b9d5f2df6724f23a596
locnht <locnht.it@gmail.com>
2025-02-13 11:11:48 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -5,9 +5,21 @@ import './style.css'
 // JSX: 1 parent
 // Fragment
 const MyComponent = () => {
+  // const locnht = "Loc"; //string
+  // const locnht = 22; //number
+  // const locnht = true; //boolean
+  // const locnht = undefined
+  // const locnht = null;
+  const locnht = [1, 2, 3];
+//   const locnht = {
+//     name: `Loc`,
+//     age: 22,
+//   }
+
   return (
     <>
-        <div>Loc Ngo-Huynh-Tan</div>
+        <div>{JSON.stringify(locnht)} Ngo-Huynh-Tan</div>
+        <div>{console.log(`LocNHT`)}</div>
         <div className="child" style={{ borderRadius: "10px" }}>Child</div>
     </>
 

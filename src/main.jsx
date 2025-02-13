2effbe5c52ed9027c320615c300027eae91206fb
locnht <locnht.it@gmail.com>
2025-02-13 20:01:32 +0700

    video 46

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import UserPage from "./pages/user.jsx";
+import ProductPage from "./pages/product.jsx";
+import "./styles/global.css";
 
 const router = createBrowserRouter([
   {
@@ -10,19 +15,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login Page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register Page</div>,
+    element: <RegisterPage />,
   },
   {
     path: "/users",
-    element: <div>Users Page</div>,
+    element: <UserPage />,
   },
   {
     path: "/products",
-    element: <div>Products Page</div>,
+    element: <ProductPage />,
   },
 ]);
 

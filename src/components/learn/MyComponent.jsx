68a5d0ad0fb646efece2e07bd0045f5db3027a33
locnht <locnht.it@gmail.com>
2025-02-13 10:59:58 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,16 @@
+import './style.css'
+
 // () => { }
 // component = html + css + js
+// JSX: 1 parent
+// Fragment
 const MyComponent = () => {
   return (
-    <div>Loc Ngo-Huynh-Tan</div> // tag
+    <>
+        <div>Loc Ngo-Huynh-Tan</div>
+        <div className="child" style={{ borderRadius: "10px" }}>Child</div>
+    </>
+
   )
 }
 

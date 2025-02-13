3000267123949ee7af00ca2d7a343231e9d6852d
locnht <locnht.it@gmail.com>
2025-02-13 12:05:56 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+  const locnht = `LocNHT`;
+  const age = 22;
+  const data = {
+    address: "Ho Chi Minh City",
+    country: "Vietnam",
+  };
+  // {key:value}
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData name={locnht} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} alt="react logo" className="logo" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,15 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  // props is an object {}
+  //   {
+  //     name: "LocNHT",
+  //     age: 22,
+  //     data: {}
+  //   }
+  const { name, age, data } = props;
+  console.log(`>>> Check props: `, props);
   return (
     <div className="todo-data">
+      <div>My name is {name}</div>
       <div>Learning React</div>
       <div>Watching Youtube</div>
     </div>

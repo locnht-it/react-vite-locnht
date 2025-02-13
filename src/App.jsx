b7c6fe823440d6106346bd5e61304f33ed553418
locnht <locnht.it@gmail.com>
2025-02-13 16:34:27 +0700

    video 40

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,9 @@ import { useState } from "react";
 
 const App = () => {
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React" },
-    { id: 2, name: "Watching Youtube" },
+    // { id: 1, name: "Learning React" },
+    // { id: 2, name: "Watching Youtube" },
   ]);
-
-  const locnht = `LocNHT`;
-  const age = 22;
-  const data = {
-    address: "Ho Chi Minh City",
-    country: "Vietnam",
-  };
 
   const addNewTodo = (name) => {
     const newTodo = {
@@ -35,7 +28,7 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData name={locnht} age={age} data={data} todoList={todoList} />
+      <TodoData todoList={todoList} />
       <div className="todo-image">
         <img src={reactLogo} alt="react logo" className="logo" />
       </div>

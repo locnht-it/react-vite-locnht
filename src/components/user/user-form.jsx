144a386bfdc0f751d0e949e4ec996a3fd0ddd004
locnht <locnht.it@gmail.com>
2025-02-14 11:20:03 +0700

    video 70

--- src/components/user/user-form.jsx
+++ src/components/user/user-form.jsx
@@ -1,6 +1,7 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
 import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
   const [fullName, setFullName] = useState("");
@@ -8,16 +9,13 @@ const UserForm = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
 
-  const handleClickBtn = () => {
-    const URL_BACKEND = `http://localhost:8080/api/v1/user`;
-    const data = {
-      fullName: fullName,
-      email: email,
-      password: password,
-      phone: phone,
-    };
-    axios.post(URL_BACKEND, data);
-    console.log(`>>> Check state: `, { fullName, email, password, phone });
+  const handleClickBtn = async () => {
+    const res = await createUserAPI(fullName, email, password, phone);
+    if (res.data)
+      notification.success({
+        message: "Create user",
+        description: "Tạo mới user thành công",
+      });
   };
 
   return (
